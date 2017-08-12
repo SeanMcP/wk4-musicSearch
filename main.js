2288654ac6d19757d4ee613d8b35051d7e6aff60
Sean McPherson <seanmcp25@gmail.com>
2017-08-12 06:21:58 -0400

    Add rounded borders on search bar and button; add smooth transition for search form

--- main.js
+++ main.js
@@ -18,74 +18,76 @@ function clearField(){
 }
 
 function searchClick(){
-
-  clearField();
-
   let query = searchBar.value;
-  let altQuery = query.split(' ').join('+');
 
-  fetch('https://itunes.apple.com/search?term=' + altQuery + '&limit=16')
-    // Data is fetched and we get a promise.
-    .then(
-      // The promise returns a response from the server.
-      function(response) {
-        // We process the response accordingly.
-        if (response.status !== 200) {
-          console.log(response.status);
-          return;
+  if(query){
+    
+    clearField();
+    let altQuery = query.split(' ').join('+');
+
+    fetch('https://itunes.apple.com/search?term=' + altQuery + '&limit=16')
+      // Data is fetched and we get a promise.
+      .then(
+        // The promise returns a response from the server.
+        function(response) {
+          // We process the response accordingly.
+          if (response.status !== 200) {
+            console.log(response.status);
+            return;
+          }
+          response.json().then(function(data) {
+            console.log('Here is the data:', data);
+
+            document.getElementById('searchForm').style.marginTop = '40px';
+
+            resultsGrid.id = 'resultsGrid';
+
+            document.getElementById('container').appendChild(resultsGrid);
+
+            let resultHead = document.createElement('h1');
+            resultHead.id = 'resultHead';
+            resultHead.textContent = 'Results for: ' + query;
+
+            resultsGrid.appendChild(resultHead);
+
+            for(var i = 0; i < data.results.length; i++) {
+
+              let result = document.createElement('article');
+              result.setAttribute('class', 'resultItem');
+              result.id = i;
+
+              let bgImg = data.results[i].artworkUrl100;
+
+              result.style.backgroundImage = 'url(' + bgImg + ')';
+
+              result.innerHTML +=
+              `
+              <h2>${data.results[i].trackName}</h2>
+              <h3>${data.results[i].artistName}</h3>
+              `
+
+              result.addEventListener('click', function(event){
+                playMusic(event.target.id);
+              });
+
+              resultsGrid.appendChild(result);
+
+            }
+            function playMusic(indexStr){
+              let index = Number(indexStr);
+              let player = document.getElementById('musicPlayer');
+              let musicUrl = data.results[index].previewUrl;
+              player.setAttribute('src', musicUrl);
+              console.log(player);
+              player.play();
+            }
+          });
         }
-        response.json().then(function(data) {
-          console.log('Here is the data:', data);
-
-          document.getElementById('searchForm').style.marginTop = '40px';
-
-          resultsGrid.id = 'resultsGrid';
-
-          document.getElementById('container').appendChild(resultsGrid);
-
-          let resultHead = document.createElement('h1');
-          resultHead.id = 'resultHead';
-          resultHead.textContent = 'Results for: ' + query;
-
-          resultsGrid.appendChild(resultHead);
-
-          for(var i = 0; i < data.results.length; i++) {
-
-            let result = document.createElement('article');
-            result.setAttribute('class', 'resultItem');
-            result.id = i;
-
-            let bgImg = data.results[i].artworkUrl100;
-
-            result.style.backgroundImage = 'url(' + bgImg + ')';
-
-            result.innerHTML +=
-            `
-            <h2>${data.results[i].trackName}</h2>
-            <h3>${data.results[i].artistName}</h3>
-            `
-
-            result.addEventListener('click', function(event){
-              playMusic(event.target.id);
-            });
-
-            resultsGrid.appendChild(result);
-
-          }
-          function playMusic(indexStr){
-            let index = Number(indexStr);
-            let player = document.getElementById('musicPlayer');
-            let musicUrl = data.results[index].previewUrl;
-            player.setAttribute('src', musicUrl);
-            console.log(player);
-            player.play();
-          }
-        });
-      }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
+      )
+      .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
+  }
 }
 
 searchButton.addEventListener('click', searchClick);
